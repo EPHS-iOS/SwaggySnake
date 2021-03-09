f9d0ed9de8f90e75fb44f27982101bd7162d0193
Evan <Evankrainess@gmail.com>
2021-03-09 11:21:07 -0600

    for abhay

--- SnakeGame/GameScene.swift
+++ SnakeGame/GameScene.swift
@@ -3,22 +3,17 @@
 //  SnakeGame
 //
 //  Created by 64003232 on 2/22/21.
-//
 import SpriteKit
 import GameplayKit
 class GameScene: SKScene {
-    
     var theSprite:SKSpriteNode = SKSpriteNode()
     var point1:CGPoint = CGPoint.zero
     var point2:CGPoint = CGPoint.zero
     var Snake = SKSpriteNode()
-    
     let cam = SKCameraNode()
     let player = SKSpriteNode()
-    
     var hasSwag = false
     var originalSwagLocation = CGPoint()
-    
     override func didMove(to view: SKView){
         Snake = self.childNode(withName: "Snake") as! SKSpriteNode
         self.camera = cam
@@ -28,23 +23,25 @@ class GameScene: SKScene {
         border.restitution = 0
         self.physicsBody = border
     }
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if !hasSwag{
-            if let touch = touches.first{
-                let touchLocation = touch.location(in: self)
-                let touched = nodes(at: touchLocation)
-                if !touched.isEmpty{
-                    for node in touched{
-                        if let sprite = node as? SKSpriteNode{
-                            if sprite == Snake{
-                                Snake.position = touchLocation
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+    /*
+     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+     if !hasSwag{
+     if let touch = touches.first{
+     let touchLocation = touch.location(in: self)
+     let touched = nodes(at: touchLocation)
+     if !touched.isEmpty{
+     for node in touched{
+     if let sprite = node as? SKSpriteNode{
+     if sprite == Snake{
+     Snake.position = touchLocation
+     }
+     }
+     }
+     }
+     }
+     }
+     }
+     */
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         if !hasSwag{
             if let touch = touches.first{
@@ -54,8 +51,8 @@ class GameScene: SKScene {
                     for node in touched{
                         if let sprite = node as? SKSpriteNode{
                             if sprite == Snake{
-                                let dx = originalSwagLocation.x - touchLocation.x
-                                let dy = originalSwagLocation.y - touchLocation.y
+                                let dx = (originalSwagLocation.x - touchLocation.x)/2.5
+                                let dy = (originalSwagLocation.y - touchLocation.y)*25
                                 let impulse = CGVector(dx: dx, dy: dy)
                                 Snake.physicsBody?.applyImpulse(impulse)
                                 hasSwag = false
@@ -69,23 +66,29 @@ class GameScene: SKScene {
         //let location = touch.location(in: self)
         //Snake.run(SKAction.applyImpulse(CGVector(dx: 0.003, dy: 0.02), duration: 0.2))
     }
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if !hasSwag{
-            if let touch = touches.first{
-                let touchLocation = touch.location(in: self)
-                let touched = nodes(at: touchLocation)
-                if !touched.isEmpty{
-                    for node in touched{
-                        if let sprite = node as? SKSpriteNode{
-                            if sprite == Snake{
-                                Snake.position = touchLocation
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+    /*
+     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+     if !hasSwag{
+     if let touch = touches.first{
+     let touchLocation = touch.location(in: self)
+     let touched = nodes(at: touchLocation)
+     if !touched.isEmpty{
+     for node in touched{
+     if let sprite = node as? SKSpriteNode{
+     if sprite == Snake{
+     let dx = originalSwagLocation.x - touchLocation.x
+     let dy = originalSwagLocation.y - touchLocation.y
+     let impulse = CGVector(dx: dx, dy: dy)
+     Snake.physicsBody?.applyImpulse(impulse)
+     hasSwag = false
+     }
+     }
+     }
+     }
+     }
+     }
+     }
+     */
     override func update(_ currentTime: CFTimeInterval){
         /* Called before each frame is rendered */
         cam.position.y = Snake.position.y
