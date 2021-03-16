60c5158c6a65d82c7e5a4eab1704b9d51ac9dc09
Evan <Evankrainess@gmail.com>
2021-03-16 12:00:52 -0500

    W

--- SnakeGame/GameScene.swift
+++ SnakeGame/GameScene.swift
@@ -15,6 +15,9 @@ class GameScene: SKScene {
   let player = SKSpriteNode()
   var hasSwag = false
   var originalSwagLocation = CGPoint()
+  var butpress = false
+  var but2press = false
+  var blpress = false
   override func didMove(to view: SKView){
     butt = self.childNode(withName: "butt") as! SKSpriteNode
     butt2 = self.childNode(withName: "butt2") as! SKSpriteNode
@@ -27,42 +30,52 @@ class GameScene: SKScene {
     border.restitution = 1
     self.physicsBody = border
   }
-  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-    if !hasSwag{
-      if let touch = touches.first{
-        let touchLocation = touch.location(in: self)
-        let touched = nodes(at: touchLocation)
-        if !touched.isEmpty{
-          for node in touched{
-            if let sprite = node as? SKSpriteNode{
-              if sprite == Snake{
-                let dx = (originalSwagLocation.x - touchLocation.x)/2.5
-                let dy = (originalSwagLocation.y - touchLocation.y)*25
-                let impulse = CGVector(dx: dx, dy: dy)
-                Snake.physicsBody?.applyImpulse(impulse)
-                hasSwag = false
-              }
-            }
-          }
-        }
-      }
-    }
-  }
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    for touch in touches {
-      let location = touch.location(in: self)
-      let touchedNode = atPoint(location)
-      if touchedNode == butt{
-        arr.zRotation = arr.zRotation + 0.2
-      } else if touchedNode == butt2{
-        arr.zRotation = arr.zRotation - 0.2
-      } else if touchedNode == blastOff{
-        Snake.physicsBody?.applyImpulse(CGVector(dx: (arr.size.height*sin(arr.zRotation)), dy: (arr.size.height*cos(arr.zRotation))))
-      }
+    let touche = touches.first!
+    if butt.contains(touche.location(in: self)){
+      butpress = true
+    }
+    if butt2.contains(touche.location(in: self)){
+      but2press = true
+    }
+    if blastOff.contains(touche.location(in: self)){
+      blpress = true
+    }
+    /*
+     for touch in touches {
+     let location = touch.location(in: self)
+     let touchedNode = atPoint(location)
+     if touchedNode == butt{
+     arr.zRotation = arr.zRotation + 0.2
+     } else if touchedNode == butt2{
+     arr.zRotation = arr.zRotation - 0.2
+     } else if touchedNode == blastOff{
+     Snake.physicsBody?.applyImpulse(CGVector(dx: (arr.size.height*sin(-arr.zRotation)), dy: (arr.size.height*cos(-arr.zRotation))))
+     }
+     }
+     */
+  }
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    let touch = touches.first!
+    if butt.contains(touch.location(in: self)){
+      butpress = false
+    }
+    if butt2.contains(touch.location(in: self)){
+      but2press = false
+    }
+    if blastOff.contains(touch.location(in: self)){
+      Snake.physicsBody?.applyImpulse(CGVector(dx: (arr.size.height*sin(-arr.zRotation)), dy: (arr.size.height*cos(-arr.zRotation))))
+      blpress = false
     }
   }
   override func update(_ currentTime: CFTimeInterval){
     /* Called before each frame is rendered */
+    if butpress == true{
+      arr.zRotation = arr.zRotation + 0.02
+    }
+    if but2press == true{
+      arr.zRotation = arr.zRotation - 0.02
+    }
     cam.position.y = Snake.position.y
     cam.position.x=CGFloat(-2028.34)
     butt.position.y = Snake.position.y - 400
