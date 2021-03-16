f8c7267ffe9ebf5c9ffc88f003a78b450e3206a7
Abhay Upadhyaya <64004046@ep-student.org>
2021-03-16 12:00:00 -0500

    a

--- SnakeGame/GameScene.swift
+++ SnakeGame/GameScene.swift
@@ -6,7 +6,10 @@
 import SpriteKit
 import GameplayKit
 class GameScene: SKScene {
+<<<<<<< Updated upstream
     
+=======
+>>>>>>> Stashed changes
     var Snake = SKSpriteNode()
     var butt = SKSpriteNode()
     var butt2 = SKSpriteNode()
@@ -16,20 +19,32 @@ class GameScene: SKScene {
     let player = SKSpriteNode()
     var hasSwag = false
     var originalSwagLocation = CGPoint()
+<<<<<<< Updated upstream
     
     override func didMove(to view: SKView){
         
+=======
+    var butpress = false
+    var but2press = false
+    var blpress = false
+    
+    override func didMove(to view: SKView){
+>>>>>>> Stashed changes
         butt = self.childNode(withName: "butt") as! SKSpriteNode
         butt2 = self.childNode(withName: "butt2") as! SKSpriteNode
         blastOff = self.childNode(withName: "blastOff") as! SKSpriteNode
         Snake = self.childNode(withName: "Snake") as! SKSpriteNode
         arr = self.childNode(withName: "arr") as! SKSpriteNode
+<<<<<<< Updated upstream
         
+=======
+>>>>>>> Stashed changes
         self.camera = cam
         let border = SKPhysicsBody(edgeLoopFrom: self.frame)
         border.friction = 0
         border.restitution = 1
         self.physicsBody = border
+<<<<<<< Updated upstream
         
     }
     
@@ -87,11 +102,102 @@ class GameScene: SKScene {
         /* Called before each frame is rendered */
         cam.position.y = Snake.position.y
         cam.position.x=CGFloat(-430)
+=======
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let touche = touches.first!
+        
+        if butt.contains(touche.location(in: self)){
+            
+            butpress = true
+            
+        }
+        
+        if butt2.contains(touche.location(in: self)){
+            
+            but2press = true
+            
+        }
+        
+        if blastOff.contains(touche.location(in: self)){
+            
+            blpress = true
+            
+        }
+        
+        /*
+         
+         for touch in touches {
+         let location = touch.location(in: self)
+         let touchedNode = atPoint(location)
+         if touchedNode == butt{
+         arr.zRotation = arr.zRotation + 0.2
+         } else if touchedNode == butt2{
+         arr.zRotation = arr.zRotation - 0.2
+         } else if touchedNode == blastOff{
+         Snake.physicsBody?.applyImpulse(CGVector(dx: (arr.size.height*sin(-arr.zRotation)), dy: (arr.size.height*cos(-arr.zRotation))))
+         }
+         }
+         */
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let touch = touches.first!
+        
+        if butt.contains(touch.location(in: self)){
+            
+            butpress = false
+            
+        }
+        
+        if butt2.contains(touch.location(in: self)){
+            
+            but2press = false
+            
+        }
+        
+        if blastOff.contains(touch.location(in: self)){
+            
+            Snake.physicsBody?.applyImpulse(CGVector(dx: (arr.size.height*sin(-arr.zRotation)), dy: (arr.size.height*cos(-arr.zRotation))))
+            
+            blpress = false
+            
+        }
+        
+    }
+    
+    override func update(_ currentTime: CFTimeInterval){
+        /* Called before each frame is rendered */
+        
+        if butpress == true{
+            
+            arr.zRotation = arr.zRotation + 0.02
+            
+        }
+        
+        if but2press == true{
+            
+            arr.zRotation = arr.zRotation - 0.02
+            
+        }
+        
+        cam.position.y = Snake.position.y
+        cam.position.x=CGFloat(-2028.34)
+>>>>>>> Stashed changes
         butt.position.y = Snake.position.y - 400
         butt2.position.y = Snake.position.y - 400
         blastOff.position.y = Snake.position.y - 400
         arr.position = Snake.position
+<<<<<<< Updated upstream
         
     }
+=======
+    }
+    
+>>>>>>> Stashed changes
 }
 
